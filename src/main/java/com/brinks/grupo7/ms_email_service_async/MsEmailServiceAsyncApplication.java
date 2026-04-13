715003d9832ad4806b804d9def53ac8b9f243331
GuilhermeAugustoFigueiredo <gui.figueiredo136@gmail.com>
2026-04-13 18:05:25 -0300

    initial commit

---
+++ src/main/java/com/brinks/grupo7/ms_email_service_async/MsEmailServiceAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.brinks.grupo7.ms_email_service_async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmailServiceAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmailServiceAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brinks/grupo7/ms_email_service_async/MsEmailServiceAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brinks.grupo7.ms_email_service_async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailServiceAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
