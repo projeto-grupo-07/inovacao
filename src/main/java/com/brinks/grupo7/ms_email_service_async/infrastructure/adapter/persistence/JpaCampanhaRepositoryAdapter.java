d43247e23006ec209e9c0cddd0f8544d96b70b1d
GuilhermeAugustoFigueiredo <gui.figueiredo136@gmail.com>
2026-04-13 22:01:57 -0300

    feat: adicionando camada adapter persisntence jpa

--- src/main/java/com/brinks/grupo7/ms_email_service_async/infrastructure/adapter/persistence/JpaCampanhaRepositoryAdapter.java
+++ src/main/java/com/brinks/grupo7/ms_email_service_async/infrastructure/adapter/persistence/JpaCampanhaRepositoryAdapter.java
@@ -1,4 +1,45 @@
 package com.brinks.grupo7.ms_email_service_async.infrastructure.adapter.persistence;
 
-public class JpaCampanhaRepositoryAdapter {
+import com.brinks.grupo7.ms_email_service_async.core.domain.model.Campanha;
+import com.brinks.grupo7.ms_email_service_async.core.domain.repository.CampanhaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class JpaCampanhaRepositoryAdapter implements CampanhaRepository {
+
+    private final SpringDataCampanhaRepository springDataCampanhaRepository;
+
+    public JpaCampanhaRepositoryAdapter(SpringDataCampanhaRepository springDataCampanhaRepository) {
+        this.springDataCampanhaRepository = springDataCampanhaRepository;
+    }
+
+    @Override
+    public Campanha salvar(Campanha campanha) {
+        CampanhaEntity entity = CampanhaEntity.fromDomain(campanha);
+        CampanhaEntity salvaEntity = springDataCampanhaRepository.save(entity);
+        return salvaEntity.toDomain();
+    }
+
+    @Override
+    public Optional<Campanha> buscarPorId(UUID id) {
+        return springDataCampanhaRepository.findById(id).map(CampanhaEntity::toDomain);
+    }
+
+    @Override
+    public List<Campanha> buscarTodas() {
+        return springDataCampanhaRepository.findAll().stream()
+                .map(CampanhaEntity::toDomain)
+                .toList();
+    }
+
+    @Override
+    public void deletar(UUID id) {
+        springDataCampanhaRepository.deleteById(id);
+    }
 }
--- src/main/java/com/brinks/grupo7/ms_email_service_async/infrastructure/adapter/persistence/SpringDataCampanhaRepository.java
+++ src/main/java/com/brinks/grupo7/ms_email_service_async/infrastructure/adapter/persistence/SpringDataCampanhaRepository.java
@@ -1,4 +1,8 @@
 package com.brinks.grupo7.ms_email_service_async.infrastructure.adapter.persistence;
 
-public interface SpringDataCampanhaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SpringDataCampanhaRepository extends JpaRepository<CampanhaEntity, UUID> {
 }
